1dd084590399a11be3c24da672bec59edeccdf7f
jj009 <xuqingxuan_0702@163.com>
2016-05-23 12:02:24 +0800

    ftp上传

---
+++ src/main/java/com/jj/util/FtpUtil.java
@@ -0,0 +1,65 @@
+package com.jj.util;
+
+import org.apache.camel.converter.stream.FileInputStreamCache;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+import org.apache.log4j.Logger;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * Created by huaren on 16/5/23.
+ */
+public class FtpUtil {
+    public static Logger logger= Logger.getLogger(FtpUtil.class);
+    private  FTPClient ftp;
+
+    private  boolean connect(String path,String addr,int port,String username,String password) throws Exception {
+        boolean result = false;
+        ftp = new FTPClient();
+        int reply;
+        ftp.connect(addr,port);
+        ftp.login(username,password);
+        ftp.setFileType(FTPClient.BINARY_FILE_TYPE);
+        ftp.setDataTimeout(60000);
+        reply = ftp.getReplyCode();
+        if (!FTPReply.isPositiveCompletion(reply)) {
+            ftp.disconnect();
+            return result;
+        }
+        ftp.changeWorkingDirectory(path);
+        result = true;
+        return result;
+    }
+    /**
+     *
+     * @param file 上传的文件或文件夹
+     * @throws Exception
+     */
+    private void upload(File file) throws Exception{
+        if(file.isDirectory()){
+            ftp.makeDirectory(file.getName());
+            ftp.changeWorkingDirectory(file.getName());
+            String[] files = file.list();
+            for (int i = 0; i < files.length; i++) {
+                File file1 = new File(file.getPath()+File.separator+files[i] );
+                if(file1.isDirectory()){
+                    upload(file1);
+                    ftp.changeToParentDirectory();
+                }else{
+                    File file2 = new File(file.getPath()+File.separator+files[i]);
+                    FileInputStream input = new FileInputStream(file2);
+                    ftp.storeFile(file2.getName(), input);
+                    input.close();
+                }
+            }
+        }else{
+            File file2 = new File(file.getPath());
+            FileInputStream input = new FileInputStream(file2);
+            ftp.storeFile(file2.getName(), input);
+            input.close();
+        }
+    }
+}
